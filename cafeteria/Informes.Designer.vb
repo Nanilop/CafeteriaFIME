5f1e2992d8f2a497a80123cbcdc3e73b3babc041
DANIELA LOPEZ RODRIGUEZ <daniela.lop@uadec.edu.mx>
2023-11-20 16:06:54 -0600

    informes parte 2

--- cafeteria/Informes.Designer.vb
+++ cafeteria/Informes.Designer.vb
@@ -23,50 +23,155 @@ Partial Class Informes
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.dgvInformes = New System.Windows.Forms.DataGridView()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.cbInformes = New System.Windows.Forms.ComboBox()
+        Me.btnBuscarInformes = New System.Windows.Forms.PictureBox()
+        Me.btnExportaInfo = New System.Windows.Forms.PictureBox()
+        Me.dtFinInfo = New System.Windows.Forms.DateTimePicker()
+        Me.dtInicioInfome = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.chkInicioInfo = New System.Windows.Forms.CheckBox()
+        Me.chkfinInfo = New System.Windows.Forms.CheckBox()
         CType(Me.dgvInformes, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnBuscarInformes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnExportaInfo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvInformes
         '
         Me.dgvInformes.AllowUserToAddRows = False
         Me.dgvInformes.AllowUserToDeleteRows = False
+        Me.dgvInformes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.dgvInformes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvInformes.Location = New System.Drawing.Point(12, 119)
+        Me.dgvInformes.Location = New System.Drawing.Point(12, 101)
         Me.dgvInformes.Name = "dgvInformes"
         Me.dgvInformes.ReadOnly = True
         Me.dgvInformes.RowHeadersVisible = False
-        Me.dgvInformes.Size = New System.Drawing.Size(810, 329)
+        Me.dgvInformes.Size = New System.Drawing.Size(810, 384)
         Me.dgvInformes.TabIndex = 0
         '
-        'PictureBox1
+        'cbInformes
         '
-        Me.PictureBox1.Image = Global.cafeteria.My.Resources.Resources.excel
-        Me.PictureBox1.Location = New System.Drawing.Point(774, 64)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(48, 49)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 1
-        Me.PictureBox1.TabStop = False
+        Me.cbInformes.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbInformes.FormattingEnabled = True
+        Me.cbInformes.Items.AddRange(New Object() {"Reporte de Venta", "Reporte de Historial de Usuarios", "Reporte de Inventario"})
+        Me.cbInformes.Location = New System.Drawing.Point(512, 63)
+        Me.cbInformes.Name = "cbInformes"
+        Me.cbInformes.Size = New System.Drawing.Size(225, 27)
+        Me.cbInformes.TabIndex = 2
+        '
+        'btnBuscarInformes
+        '
+        Me.btnBuscarInformes.BackColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.btnBuscarInformes.Image = Global.cafeteria.My.Resources.Resources.buscar1
+        Me.btnBuscarInformes.Location = New System.Drawing.Point(743, 59)
+        Me.btnBuscarInformes.Name = "btnBuscarInformes"
+        Me.btnBuscarInformes.Size = New System.Drawing.Size(34, 34)
+        Me.btnBuscarInformes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnBuscarInformes.TabIndex = 3
+        Me.btnBuscarInformes.TabStop = False
+        '
+        'btnExportaInfo
+        '
+        Me.btnExportaInfo.BackColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.btnExportaInfo.Image = Global.cafeteria.My.Resources.Resources.excel
+        Me.btnExportaInfo.Location = New System.Drawing.Point(788, 59)
+        Me.btnExportaInfo.Name = "btnExportaInfo"
+        Me.btnExportaInfo.Size = New System.Drawing.Size(34, 34)
+        Me.btnExportaInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnExportaInfo.TabIndex = 1
+        Me.btnExportaInfo.TabStop = False
+        '
+        'dtFinInfo
+        '
+        Me.dtFinInfo.Enabled = False
+        Me.dtFinInfo.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtFinInfo.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtFinInfo.Location = New System.Drawing.Point(314, 63)
+        Me.dtFinInfo.Name = "dtFinInfo"
+        Me.dtFinInfo.Size = New System.Drawing.Size(101, 27)
+        Me.dtFinInfo.TabIndex = 4
+        '
+        'dtInicioInfome
+        '
+        Me.dtInicioInfome.Enabled = False
+        Me.dtInicioInfome.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtInicioInfome.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtInicioInfome.Location = New System.Drawing.Point(144, 63)
+        Me.dtInicioInfome.Name = "dtInicioInfome"
+        Me.dtInicioInfome.Size = New System.Drawing.Size(101, 27)
+        Me.dtInicioInfome.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label3.Location = New System.Drawing.Point(421, 63)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 26)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Informe:"
+        '
+        'chkInicioInfo
+        '
+        Me.chkInicioInfo.AutoSize = True
+        Me.chkInicioInfo.BackColor = System.Drawing.Color.Transparent
+        Me.chkInicioInfo.Font = New System.Drawing.Font("Calibri", 15.75!)
+        Me.chkInicioInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.chkInicioInfo.Location = New System.Drawing.Point(61, 61)
+        Me.chkInicioInfo.Name = "chkInicioInfo"
+        Me.chkInicioInfo.Size = New System.Drawing.Size(77, 30)
+        Me.chkInicioInfo.TabIndex = 9
+        Me.chkInicioInfo.Text = "Inicio"
+        Me.chkInicioInfo.UseVisualStyleBackColor = False
+        '
+        'chkfinInfo
+        '
+        Me.chkfinInfo.AutoSize = True
+        Me.chkfinInfo.BackColor = System.Drawing.Color.Transparent
+        Me.chkfinInfo.Font = New System.Drawing.Font("Calibri", 15.75!)
+        Me.chkfinInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.chkfinInfo.Location = New System.Drawing.Point(251, 61)
+        Me.chkfinInfo.Name = "chkfinInfo"
+        Me.chkfinInfo.Size = New System.Drawing.Size(57, 30)
+        Me.chkfinInfo.TabIndex = 10
+        Me.chkfinInfo.Text = "Fin"
+        Me.chkfinInfo.UseVisualStyleBackColor = False
         '
         'Informes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(834, 460)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(834, 494)
+        Me.Controls.Add(Me.chkfinInfo)
+        Me.Controls.Add(Me.chkInicioInfo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.dtInicioInfome)
+        Me.Controls.Add(Me.dtFinInfo)
+        Me.Controls.Add(Me.btnBuscarInformes)
+        Me.Controls.Add(Me.cbInformes)
+        Me.Controls.Add(Me.btnExportaInfo)
         Me.Controls.Add(Me.dgvInformes)
         Me.Location = New System.Drawing.Point(0, 35)
         Me.Name = "Informes"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Informes"
         CType(Me.dgvInformes, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnBuscarInformes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnExportaInfo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents dgvInformes As DataGridView
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnExportaInfo As PictureBox
+    Friend WithEvents cbInformes As ComboBox
+    Friend WithEvents btnBuscarInformes As PictureBox
+    Friend WithEvents dtFinInfo As DateTimePicker
+    Friend WithEvents dtInicioInfome As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents chkInicioInfo As CheckBox
+    Friend WithEvents chkfinInfo As CheckBox
 End Class
--- cafeteria/Informes.vb
+++ cafeteria/Informes.vb
@@ -1,4 +1,5 @@
-﻿Imports BOCafeteria
+﻿Imports System.Data.SqlClient
+Imports BOCafeteria
 Imports Excel = Microsoft.Office.Interop.Excel
 
 
@@ -16,7 +17,7 @@ Public Class Informes
         usuario = user
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles btnExportaInfo.Click
         Dim dialogoGuardar = New SaveFileDialog()
         dialogoGuardar.Filter = "Hoja de cálculo de Microsoft Excel (*.xlsx)|*.xlsx"
         If (dialogoGuardar.ShowDialog() = DialogResult.Cancel) Then
@@ -50,5 +51,72 @@ Public Class Informes
         libros_trabajo.SaveAs(dialogoGuardar.FileName)
         libros_trabajo.Close(True)
         aplicacion.Quit()
+    End Sub
+
+    Private Sub btnBuscarInformes_Click(sender As Object, e As EventArgs) Handles btnBuscarInformes.Click
+        If cbInformes.SelectedIndex = 0 Then
+            'Reporte de Venta
+            Dim consult As String = ""
+            If chkfinInfo.Checked And chkInicioInfo.Checked Then
+                consult = "Select * from InformeVenta where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' and convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            ElseIf chkInicioInfo.Checked Then
+                consult = "Select * from InformeVenta where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' "
+
+            ElseIf chkfinInfo.Checked Then
+                consult = "Select * from InformeVenta where convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            Else
+                consult = "Select * from InformeVenta"
+            End If
+            Dim dt As New DataTable
+
+            Try
+
+                Using sql As New SqlConnection("Data Source=DESKTOP-CUOAPA9\SQLEXPRESS;Initial Catalog=Proyecto;Integrated Security=True")
+                    sql.Open()
+                    Using cmd As New SqlCommand
+                        With cmd
+                            .Connection = sql
+                            .CommandType = CommandType.Text
+                            .CommandText = consult
+                        End With
+
+                        Using da = New SqlDataAdapter(cmd)
+                            da.Fill(dt)
+                        End Using
+                    End Using
+                End Using
+
+            Catch ex As SqlException
+
+                MessageBox.Show(ex.Message)
+
+            End Try
+            dgvInformes.DataSource = dt
+            dgvInformes.Columns("Fecha").AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+
+        ElseIf cbInformes.SelectedIndex = 1 Then
+            'Reporte de Historial de Usuarios
+
+        ElseIf cbInformes.SelectedIndex = 2 Then
+            'Reporte de Inventario
+
+        End If
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkInicioInfo.CheckedChanged
+        If chkInicioInfo.Checked Then
+            dtInicioInfome.Enabled = True
+        Else
+            dtInicioInfome.Enabled = False
+        End If
+    End Sub
+
+    Private Sub chkfinInfo_CheckedChanged(sender As Object, e As EventArgs) Handles chkfinInfo.CheckedChanged
+        If chkfinInfo.Checked Then
+            dtFinInfo.Enabled = True
+        Else
+            dtFinInfo.Enabled = False
+        End If
     End Sub
 End Class
