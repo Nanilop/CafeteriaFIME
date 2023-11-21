7e04e4ca0a74f5069365fb30e3ed5f9f9a570809
DANIELA LOPEZ RODRIGUEZ <daniela.lop@uadec.edu.mx>
2023-11-20 19:19:50 -0600

    Informes pantalla terminada

--- cafeteria/Informes.vb
+++ cafeteria/Informes.vb
@@ -33,24 +33,71 @@ Public Class Informes
         hoja_trabajo = CType(libros_trabajo.Worksheets.Item(1), Excel.Worksheet)
 
         Dim a As Integer = 2
-        'hoja_trabajo.Cells(1, 1) = "Nombre"
-        'hoja_trabajo.Cells(1, 2) = "Usuario"
-        'hoja_trabajo.Cells(1, 3) = "Lenguaje"
-        'hoja_trabajo.Cells(1, 4) = "Archivo"
-        'hoja_trabajo.Cells(1, 5) = "Fecha_Hora"
-        'For Each fila As DataGridViewRow In dgvInformes.Rows
+        If cbInformes.SelectedIndex = 0 Then
+            'Reporte de Venta
+            hoja_trabajo.Cells(1, 1) = "A Nombre de"
+            hoja_trabajo.Cells(1, 2) = "Fecha"
+            hoja_trabajo.Cells(1, 3) = "Total"
+            hoja_trabajo.Cells(1, 4) = "Pago"
+            hoja_trabajo.Cells(1, 5) = "Cambio"
+            hoja_trabajo.Cells(1, 6) = "Vendedor"
+            hoja_trabajo.Cells(1, 7) = "Usuario"
+            hoja_trabajo.Cells(1, 8) = "Telefono"
+            hoja_trabajo.Cells(1, 9) = "Estatus"
+            For Each fila As DataGridViewRow In dgvInformes.Rows
 
-        '    hoja_trabajo.Cells(a, 1) = fila.Cells(0).Value.ToString()
-        '    hoja_trabajo.Cells(a, 2) = fila.Cells(1).Value.ToString()
-        '    hoja_trabajo.Cells(a, 3) = fila.Cells(2).Value.ToString()
-        '    hoja_trabajo.Cells(a, 4) = fila.Cells(3).Value.ToString()
-        '    hoja_trabajo.Cells(a, 5) = fila.Cells(4).Value.ToString()
-        '    a += 1
-
-        'Next
+                hoja_trabajo.Cells(a, 1) = fila.Cells(0).Value.ToString()
+                hoja_trabajo.Cells(a, 2) = fila.Cells(1).Value.ToString()
+                hoja_trabajo.Cells(a, 3) = fila.Cells(2).Value.ToString()
+                hoja_trabajo.Cells(a, 4) = fila.Cells(3).Value.ToString()
+                hoja_trabajo.Cells(a, 5) = fila.Cells(4).Value.ToString()
+                hoja_trabajo.Cells(a, 6) = fila.Cells(5).Value.ToString()
+                hoja_trabajo.Cells(a, 7) = fila.Cells(6).Value.ToString()
+                hoja_trabajo.Cells(a, 8) = fila.Cells(7).Value.ToString()
+                hoja_trabajo.Cells(a, 9) = fila.Cells(8).Value.ToString()
+                a += 1
+            Next
+        ElseIf cbInformes.SelectedIndex = 1 Then
+            'Reporte de Historial de Usuarios
+            hoja_trabajo.Cells(1, 1) = "Fecha"
+            hoja_trabajo.Cells(1, 2) = "Empleado"
+            hoja_trabajo.Cells(1, 3) = "Telefono"
+            hoja_trabajo.Cells(1, 4) = "Email"
+            hoja_trabajo.Cells(1, 5) = "Usuario"
+            hoja_trabajo.Cells(1, 6) = "Accion"
+            For Each fila As DataGridViewRow In dgvInformes.Rows
+                hoja_trabajo.Cells(a, 1) = fila.Cells(0).Value.ToString()
+                hoja_trabajo.Cells(a, 2) = fila.Cells(1).Value.ToString()
+                hoja_trabajo.Cells(a, 3) = fila.Cells(2).Value.ToString()
+                hoja_trabajo.Cells(a, 4) = fila.Cells(3).Value.ToString()
+                hoja_trabajo.Cells(a, 5) = fila.Cells(4).Value.ToString()
+                hoja_trabajo.Cells(a, 6) = fila.Cells(5).Value.ToString()
+                a += 1
+            Next
+        ElseIf cbInformes.SelectedIndex = 2 Then
+            'Reporte de Inventarios
+            hoja_trabajo.Cells(1, 1) = "Fecha"
+            hoja_trabajo.Cells(1, 2) = "Cantidad"
+            hoja_trabajo.Cells(1, 3) = "Producto"
+            hoja_trabajo.Cells(1, 4) = "Accion"
+            hoja_trabajo.Cells(1, 5) = "Empleado"
+            hoja_trabajo.Cells(1, 6) = "Telefono"
+            hoja_trabajo.Cells(1, 7) = "Usuario"
+            For Each fila As DataGridViewRow In dgvInformes.Rows
+                hoja_trabajo.Cells(a, 1) = fila.Cells(0).Value.ToString()
+                hoja_trabajo.Cells(a, 2) = fila.Cells(1).Value.ToString()
+                hoja_trabajo.Cells(a, 3) = fila.Cells(2).Value.ToString()
+                hoja_trabajo.Cells(a, 4) = fila.Cells(3).Value.ToString()
+                hoja_trabajo.Cells(a, 5) = fila.Cells(4).Value.ToString()
+                hoja_trabajo.Cells(a, 6) = fila.Cells(5).Value.ToString()
+                hoja_trabajo.Cells(a, 7) = fila.Cells(6).Value.ToString()
+                a += 1
+            Next
+        End If
         libros_trabajo.SaveAs(dialogoGuardar.FileName)
         libros_trabajo.Close(True)
         aplicacion.Quit()
+        MessageBox.Show("Exportado")
     End Sub
 
     Private Sub btnBuscarInformes_Click(sender As Object, e As EventArgs) Handles btnBuscarInformes.Click
@@ -96,9 +143,83 @@ Public Class Informes
 
         ElseIf cbInformes.SelectedIndex = 1 Then
             'Reporte de Historial de Usuarios
+            Dim consult As String = ""
+            If chkfinInfo.Checked And chkInicioInfo.Checked Then
+                consult = "Select * from InformeHistorialUsuarios where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' and convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            ElseIf chkInicioInfo.Checked Then
+                consult = "Select * from InformeHistorialUsuarios where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' "
+
+            ElseIf chkfinInfo.Checked Then
+                consult = "Select * from InformeHistorialUsuarios where convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            Else
+                consult = "Select * from InformeHistorialUsuarios"
+            End If
+            Dim dt As New DataTable
+
+            Try
+
+                Using sql As New SqlConnection("Data Source=DESKTOP-CUOAPA9\SQLEXPRESS;Initial Catalog=Proyecto;Integrated Security=True")
+                    sql.Open()
+                    Using cmd As New SqlCommand
+                        With cmd
+                            .Connection = sql
+                            .CommandType = CommandType.Text
+                            .CommandText = consult
+                        End With
+
+                        Using da = New SqlDataAdapter(cmd)
+                            da.Fill(dt)
+                        End Using
+                    End Using
+                End Using
+
+            Catch ex As SqlException
+
+                MessageBox.Show(ex.Message)
+
+            End Try
+            dgvInformes.DataSource = dt
+            dgvInformes.Columns("Fecha").AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
 
         ElseIf cbInformes.SelectedIndex = 2 Then
             'Reporte de Inventario
+            Dim consult As String = ""
+            If chkfinInfo.Checked And chkInicioInfo.Checked Then
+                consult = "Select * from InformeInventario where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' and convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            ElseIf chkInicioInfo.Checked Then
+                consult = "Select * from InformeInventario where convert(date,Fecha)>='" + dtInicioInfome.Value.ToString("yyyy/MM/dd") + "' "
+
+            ElseIf chkfinInfo.Checked Then
+                consult = "Select * from InformeInventario where convert(date,Fecha)<='" + dtFinInfo.Value.ToString("yyyy/MM/dd") + "'"
+            Else
+                consult = "Select * from InformeInventario"
+            End If
+            Dim dt As New DataTable
+
+            Try
+
+                Using sql As New SqlConnection("Data Source=DESKTOP-CUOAPA9\SQLEXPRESS;Initial Catalog=Proyecto;Integrated Security=True")
+                    sql.Open()
+                    Using cmd As New SqlCommand
+                        With cmd
+                            .Connection = sql
+                            .CommandType = CommandType.Text
+                            .CommandText = consult
+                        End With
+
+                        Using da = New SqlDataAdapter(cmd)
+                            da.Fill(dt)
+                        End Using
+                    End Using
+                End Using
+
+            Catch ex As SqlException
+
+                MessageBox.Show(ex.Message)
+
+            End Try
+            dgvInformes.DataSource = dt
+            dgvInformes.Columns("Fecha").AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
 
         End If
 
